22d5badb628f4f2fff080f0fb530929f798fc3b2
pratham <prathamshinde987@gmail.com>
2022-07-09 00:37:38 +0530

    added some addition patterns

--- Patterns.py
+++ Patterns.py
@@ -183,3 +183,59 @@ for i in range(1,n + 1):
     print('\n')
 
 
+# pattern 10..........................................
+
+# 6 5 4 3 2 1 
+# 6 5 4 3 2
+# 6 5 4 3
+# 6 5 4
+# 6 5
+# 6
+
+
+for i in range(1,n + 1):
+    for j in range(n,0,-1):
+        if(j >= i):
+            print(j,end = ' ')
+        else:
+            print(" ",end=' ')
+    print("\n")
+
+
+# pattern 11..........................................
+
+# 6 7 8 9 10 11
+# 5 6 7 8 9
+# 4 5 6 7
+# 3 4 5
+# 2 3
+# 1
+
+for i in range(1,n+1):
+    for j in range(1,n + 1):
+        if(j <= ((n + 1)-i)):
+            print(n - i + j,end=' ')
+        else:
+            print(' ', end=' ')
+    print("\n")
+
+
+# pattern 12..........................................
+
+# 1
+# 2 3
+# 4 5 6
+# 7 8 9 10
+# 11 12 13 14 15
+
+value = 1
+for i in range(1,n + 1):
+    for j in range(1,n + 1):
+        if(j <= i):
+            print(value,end=' ')
+            value += 1
+        else:
+            print(" ",end=' ')
+    print('\n')
+
+
